1a8af5b759428338513e75b179399467253cdaad
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-11-15 11:21:14 -0500

    added some vehicle seed data

---
+++ server/seeders/vehicleSeeds.js
@@ -0,0 +1,169 @@
+[
+    {
+      "make": "Toyota",
+      "model": "Camry",
+      "size": "Midsize",
+      "type": "Sedan",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 28,
+      "passenger_capacity": 5,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Honda",
+      "model": "CR-V",
+      "size": "Compact",
+      "type": "SUV",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 30,
+      "passenger_capacity": 5,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Ford",
+      "model": "F-150",
+      "size": "Fullsize",
+      "type": "Truck",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 20,
+      "passenger_capacity": 5,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Chevrolet",
+      "model": "Tahoe",
+      "size": "Fullsize",
+      "type": "SUV",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 18,
+      "passenger_capacity": 7,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Nissan",
+      "model": "Altima",
+      "size": "Midsize",
+      "type": "Sedan",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 29,
+      "passenger_capacity": 5,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Toyota",
+      "model": "Rav4",
+      "size": "Compact",
+      "type": "SUV",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 26,
+      "passenger_capacity": 5,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Ford",
+      "model": "Escape",
+      "size": "Compact",
+      "type": "SUV",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 28,
+      "passenger_capacity": 5,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Chevrolet",
+      "model": "Silverado",
+      "size": "Fullsize",
+      "type": "Truck",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 17,
+      "passenger_capacity": 5,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Honda",
+      "model": "Pilot",
+      "size": "Midsize",
+      "type": "SUV",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 23,
+      "passenger_capacity": 8,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Volkswagen",
+      "model": "Jetta",
+      "size": "Compact",
+      "type": "Sedan",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 30,
+      "passenger_capacity": 5,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Hyundai",
+      "model": "Santa Fe",
+      "size": "Midsize",
+      "type": "SUV",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 25,
+      "passenger_capacity": 5,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Jeep",
+      "model": "Cherokee",
+      "size": "Compact",
+      "type": "SUV",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 24,
+      "passenger_capacity": 5,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Ford",
+      "model": "Explorer",
+      "size": "Midsize",
+      "type": "SUV",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 27,
+      "passenger_capacity": 7,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Chevrolet",
+      "model": "Malibu",
+      "size": "Midsize",
+      "type": "Sedan",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 29,
+      "passenger_capacity": 5,
+      "transmission": "Automatic"
+    },
+    {
+      "make": "Toyota",
+      "model": "Highlander",
+      "size": "Midsize",
+      "type": "SUV",
+      "fuel_type": "Gasoline",
+      "doors": 4,
+      "mpg": 24,
+      "passenger_capacity": 8,
+      "transmission": "Automatic"
+    }
+    
+  ]
+  
