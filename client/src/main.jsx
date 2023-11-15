acc30c3e63a8b0af4f4e5120125dc459055e5f2d
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-11-15 13:15:10 -0500

    app.jsx, main.jsx, navtabs.jsx, and style folder add

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,43 +1,51 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/style/index.css'
+
 
 import App from './App.jsx';
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import SingleThought from './pages/SingleThought';
-import Profile from './pages/Profile';
-import ErrorPage from './pages/ErrorPage';
+import Error from './components/pages/error/Error.jsx';
+import Home from './components/pages/home/Home.jsx';
+import Contact from './components/pages/contact/Contact.jsx';
+import About from './components/pages/about/About.jsx';
+import Portfolio from './components/pages/portfolio/Portfolio.jsx';
+import Resume from './components/pages/resume/Resume.jsx';
+
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
-    errorElement: <ErrorPage />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
-        element: <Home />
-      }, {
-        path: '/login',
-        element: <Login />
-      }, {
-        path: '/signup',
-        element: <Signup />
-      }, {
-        path: '/profiles/:username',
-        element: <Profile />
-      }, {
-        path: '/me',
-        element: <Profile />
-      }, {
-        path: '/thoughts/:thoughtId',
-        element: <SingleThought />
-      }
-    ]
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-)
+);
