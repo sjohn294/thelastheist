9505ae0ddaba63a22eb00daea3554a03ae39e9a4
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-11-16 11:31:46 -0500

    server clean

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,23 +11,17 @@ const typeDefs = `
     id: ID!
     make: String!
     model: String!
+    size: String
+    type: String
     doors: Int!
-    passengerCount: Int!
+    fuel_type: String!
+    mpg: Int
+    range: Int
+    passenger_capacity: Int!
     transmission: TransmissionType!
-    fuelType: FuelType!
-    mpg: Float!
-    isPetFriendly: Boolean!
-    createdAt: String!
-    comments: [Comment]!
-  }
-  type Comment {
-    _id: ID
-    commentText: String
-    commentAuthor: String
-    createdAt: String
-  }
+      }
 
-  type Auth {
+   type Auth {
     token: ID!
     user: User
   }
@@ -43,33 +37,9 @@ const typeDefs = `
   type Mutation {
     addUser(name: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addCar(input: CreateCarInput!): Car
-    updateCar(input: UpdateCarInput!): Car
-    deleteCar(id: ID!): Car
+    
   }
 
-  input CreateCarInput {
-    make: String!
-    model: String!
-    doors: Int!
-    passengerCount: Int!
-    transmission: TransmissionType!
-    fuelType: FuelType!
-    mpg: Float!
-    isPetFriendly: Boolean!
-  }
-
-  input UpdateCarInput {
-    id: ID!
-    make: String
-    model: String
-    doors: Int
-    passengerCount: Int
-    transmission: TransmissionType
-    fuelType: FuelType
-    mpg: Float
-    isPetFriendly: Boolean
-  }
 `;
 
 module.exports = typeDefs;
