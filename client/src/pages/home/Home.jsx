4d90f6994889a5deb4b51cb4eca0ff9befe0dddf
hana reiko <hanareiko@gmail.com>
2023-11-16 10:00:13 -0500

    home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,36 +1,47 @@
 import { useQuery } from '@apollo/client';
+import React from 'react';
+import cars from '../../vehicleSeeds';
+import styles from './home.module.css'; 
 
-import ThoughtList from '../../components/ThoughtList';
-import ThoughtForm from '../../components/ThoughtForm';
 
-import { QUERY_THOUGHTS } from '../../utils/queries';
-
+const cars = [
+  { name: 'Sedan', type: 'Sedan' },
+  { name: 'SUV', type: 'SUV' },
+  { name: 'Compact', type: 'Compact' },
+  { name: 'Truck', type: 'Truck' },
+  { name: 'Hatchback', type: 'Hatchback'},
+  { name: 'Coupe', type: 'Coupe' },
+  { name: 'Wagon', type: 'Wagon' },
+  { name: 'Minivan', type: 'Minivan' },
+  { name: 'Van', type: 'Van'},
+]
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const thoughts = data?.thoughts || [];
-
   return (
+
     <main>
+      <nav class="navbar">
+      <ul>
+        <li><a href="/reservation" class="navbar-link">Reservation</a></li>
+        <li><a href="/vehicles" class="navbar-link">Vehicles</a></li>
+        <li><a href="/region" class="navbar-link">Region</a></li>
+      </ul>
+      </nav>
+
       <div className="flex-row justify-center">
-        <div
+          <div
           className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <ThoughtForm />
-        </div>
-        <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
-          )}
+          style={{ border: '1px dotted #1a1a1a' }}>
+        
+          <div className={styles.cardSection}>
+            {cars.map((car, index) => (
+              <CarCard key={index} car={car} />
+            ))}
+          </div>
         </div>
       </div>
     </main>
   );
-};
+}
 
 export default Home;
+
