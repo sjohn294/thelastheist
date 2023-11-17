6bc0e3d13b36df5e2024cbed5134ec4644cac705
hana reiko <hanareiko@gmail.com>
2023-11-17 12:22:00 -0500

    jwt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,24 @@ import { Outlet } from 'react-router-dom';
 import Nav from './NavTabs';
 // import Main from './main';
 import './style/app.css'
+import NavTabs from './NavTabs';
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
       <Nav />
+      const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    NavTabs: {
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
       <main className="main">
         <Outlet />
       </main>
