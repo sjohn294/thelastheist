65c09461cddb47fd492b0c92c893cb5b804fe2ca
sjohn294 <johnson.sam26.sj@gmail.com>
2023-11-20 15:29:47 -0500

    victypes cardimgs

--- client/src/components/VicTypes/index.jsx
+++ client/src/components/VicTypes/index.jsx
@@ -1,25 +1,26 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
-
-
-
-
-
+import SedanImage from '../../assets/car_images/1.png';
+import SuvImage from '../../assets/car_images/4.jpeg';
+import TruckImage from '../../assets/car_images/10.jpg';
+import HatchbackImage from '../../assets/car_images/42.jpg';
+import WagonImage from '../../assets/car_images/48.jpg';
+import MinivanImage from '../../assets/car_images/62.jpeg';
+import CoupeImage from '../../assets/car_images/33.jpg';
+import VanImage from '../../assets/car_images/70.jpg';
 
 
 
 export default function VicTypes() {
     const categories =[
-        {title: '', image:'', dataLink:''},
-        {title: '', image:'', dataLink:''},
-        {title: '', image:'', dataLink:''},
-        {title: '', image:'', dataLink:''},
-        {title: '', image:'', dataLink:''},
-        {title: '', image:'', dataLink:''},
-        {title: '', image:'', dataLink:''},
-        {title: '', image:'', dataLink:''},
-        {title: '', image:'', dataLink:''},
+        {title: 'Sedans', image:SedanImage, dataLink:''},
+        {title: 'SUVs', image:SuvImage, dataLink:''},
+        {title: 'Trucks', image:TruckImage, dataLink:''},
+        {title: 'Hatchbacks', image:HatchbackImage, dataLink:''},
+        {title: 'Wagons', image:WagonImage, dataLink:''},
+        {title: 'Minivans', image:MinivanImage, dataLink:''},
+        {title: 'Coupes', image:CoupeImage, dataLink:''},
+        {title: 'Vans', image:VanImage, dataLink:''},
     ]
 
     return (
